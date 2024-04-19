a54788e517c3acd845aab5d43be23be001adcdfe
Denis Slabko <debug.start@gmail.com>
2024-04-20 01:33:13 +0300

    added environment & dynamic content logic (#1)

---
+++ Assets/Scripts/Runtime/QuestObjects/AnimatedDoor.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+
+namespace EscapeRoom.QuestObjects
+{
+    /// <summary>
+    /// Script that switches door animation states
+    /// </summary>
+    public class AnimatedDoor: MonoBehaviour
+    {
+        [SerializeField] 
+        [Tooltip("The door open state on start")]
+        private bool openOnStart;
+        
+        [SerializeField] 
+        [Tooltip("Animator of the door that animates open state")]
+        private Animator animator;
+
+        [SerializeField] 
+        [Tooltip("Name of the bool parameter for open state.")]
+        private string openPropertyName = "Open";
+        
+        /// <summary>
+        /// Whether the door is open or not
+        /// </summary>
+        public bool Open
+        {
+            get => animator.GetBool(openPropertyName);
+            set => animator.SetBool(openPropertyName, value);
+        }
+
+        private void Awake()
+        {
+            Open = openOnStart;
+        }
+    }
+}
