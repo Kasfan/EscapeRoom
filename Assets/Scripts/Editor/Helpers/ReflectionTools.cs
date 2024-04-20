2fae5c9b68e0a88f5a93717e3474514475712a07
Denis Slabko <debug.start@gmail.com>
2024-04-21 02:03:44 +0300

    added automatic condition types detector & tests

---
+++ Assets/Scripts/Editor/Helpers/ReflectionTools.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace EscapeRoom.EditorScripts.Helpers
+{
+    /// <summary>
+    /// Helper methods that use reflection
+    /// </summary>
+    public abstract class ReflectionTools
+    {
+        /// <summary>
+        /// Returns all the types that can be assigned as provided type
+        /// Ignores abstract classes and generic types.
+        /// </summary>
+        /// <param name="type">target interface</param>
+        /// <param name="abstractTypes">include abstract types</param>
+        /// <param name="genericTypes">include generic types</param>
+        /// <returns>types that implement the interface.</returns>
+        public static Type[] GetTypesImplementingInterface(Type type,
+            bool abstractTypes = false,
+            bool genericTypes = false)
+        {
+            var types = AppDomain.CurrentDomain.GetAssemblies()
+                .SelectMany(s => s.GetTypes())
+                .Where(p => type.IsAssignableFrom(p)
+                            && p.IsClass
+                            && (genericTypes || !p.IsGenericType)
+                            && (abstractTypes || !p.IsAbstract)).ToArray();
+            return types;
+        }
+    }
+}
---
+++ Assets/Scripts/Tests/Editor/Helpers/ReflectionToolsTests.cs
@@ -0,0 +1,55 @@
+﻿using NUnit.Framework;
+
+namespace EscapeRoom.EditorScripts.Helpers.Tests
+{
+    /// <summary>
+    /// Tests for <see cref="ReflectionTools"/>
+    /// </summary>
+    [TestFixture(Category = "Helpers")]
+    public class ReflectionToolsTests
+    {
+
+        [TestCase( false, false, ExpectedResult = 2)]
+        [TestCase(false, true, ExpectedResult = 3)]
+        [TestCase( true, false, ExpectedResult = 3)]
+        [TestCase( true, true, ExpectedResult = 4)]
+        public int GetTypesAssignableFrom(bool abstractTypes, bool genericTypes)
+        {
+            var types = ReflectionTools.GetTypesImplementingInterface(typeof(IFoo), abstractTypes, genericTypes);
+
+            return types.Length;
+        }
+
+
+        private interface IFoo
+        {
+            
+        }
+        
+        private interface IFooDerived: IFoo
+        {
+            
+        }
+        
+        private abstract class FooAbstract: IFoo
+        {
+            
+        }
+        
+        private class Foo1: IFoo
+        {
+            
+        }
+        
+        private class Foo2: IFooDerived
+        {
+            
+        }
+        
+        private class FooGeneric<T>: IFooDerived
+        {
+            
+        }
+    }
+    
+}
