20d0b5bc4b4ab8462d1505e583e28329ea8944cd
Denis Slabko <debug.start@gmail.com>
2024-04-20 16:01:15 +0300

    assets & game scene updates (#2)

---
+++ Assets/Scripts/Runtime/QuestObjects/CrystalColor.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace EscapeRoom.QuestObjects
+{
+    public class CrystalColor: MonoBehaviour
+    {
+        /// <summary>
+        /// Color type of the crystal
+        /// </summary>
+        public enum Color
+        {
+            Red = 0,
+            Green = 1,
+            Purple = 2
+        }
+
+        [SerializeField] 
+        [Tooltip("Type of the crystal")]
+        private Color color;
+
+        [SerializeField] 
+        [Tooltip("Color materials of the crystal: 1:Red, 2:Green, 3:Purple")]
+        private Material[] colorMaterials;
+
+        [SerializeField] 
+        [Tooltip("Renderer of the crystal")]
+        private Renderer graphics;
+
+
+        private void Awake()
+        {
+            graphics.material = colorMaterials[(int)color];
+        }
+    }
+}
