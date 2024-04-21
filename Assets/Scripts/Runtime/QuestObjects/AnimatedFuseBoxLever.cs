9cc7c4cfe412b06aaf96e2166006888e678c9dc8
Denis Slabko <debug.start@gmail.com>
2024-04-21 09:58:45 +0300

    Added animated states for doors and fusebox & placed interactable objects in main scene

---
+++ Assets/Scripts/Runtime/QuestObjects/AnimatedFuseBoxLever.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace Runtime.QuestObjects
+{
+    /// <summary>
+    /// Script that switches door animation states
+    /// </summary>
+    public class AnimatedFuseBoxLever: MonoBehaviour
+    {
+        [SerializeField] 
+        [Tooltip("Animator of the door that animates on-of states")]
+        private Animator animator;
+
+        [SerializeField] 
+        [Tooltip("Name of the bool parameter for on state.")]
+        private string isOnPropertyName = "IsOn";
+
+        
+        /// <summary>s
+        /// Whether the power is On or Off
+        /// </summary>
+        public bool IsOn
+        {
+            get => animator.GetBool(isOnPropertyName);
+            set => animator.SetBool(isOnPropertyName, value);
+        }
+    }
+}
---
+++ Assets/Scripts/Runtime/QuestObjects/FuseboxLeverInteractable.cs
@@ -0,0 +1,37 @@
+﻿using EscapeRoom.QuestLogic;
+using UnityEngine;
+
+namespace EscapeRoom.Interactions
+{
+    /// <summary>
+    /// Activate interactable for FuseboxLever
+    /// </summary>
+    [RequireComponent(typeof(BoolStateProxyComponent))]
+    public class FuseboxLeverInteractable: MonoBehaviour, IActivateInteractable
+    {
+        /// <inheritdoc/>
+        public string InteractionTooltip
+        {
+            get
+            {
+                if (stateProxyComponent == null)
+                    return "Switch";
+
+                return stateProxyComponent.Value ? "Turn Off" : "Turn On";
+            }
+        }
+
+        private BoolStateProxyComponent stateProxyComponent;
+
+        private void Awake()
+        {
+            stateProxyComponent = GetComponent<BoolStateProxyComponent>();
+        }
+
+        /// <inheritdoc/>
+        public void OnActivated()
+        {
+            stateProxyComponent.Value = !stateProxyComponent.Value;
+        }
+    }
+}
